71da26ca633aeaca01cb509f78827a8fc599f99a
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-03-19 23:16:42 -0700

    Routes fixed and README updated

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Tasks-Hunter-Note-Taker
-Organize your tasks and track them till you complete them
+Organize your tasks and track them till you complete them.
+
+## How to use it:
+* Open
+* Press on (Get Started) button to navigate to notes page.
+* Add Note Title & Note Text, then you will notice save button appear at navigation bar.
+* Press save button to be added to left column.
+* To retrieve your Note, press on it on left hand column, note will appear in the right hand column.
+* Once you done your task, press on trash button to delete it.
+
+## Mock-up
+ Tasks Hunter Note Taker GitHub repo URL:
+ https://github.com/alaakouki/Tasks-Hunter-Note-Taker
+
+The following images is how Tasks Hunter NOte Taker webpage shows to
+users on desktop screens:
+![alt main-page](./public/assets/images/main%20page.png)
+![alt notes-page](./public/assets/images/notes%20page.png)
+![alt note-database-array](./public/assets/images/notes%20database%20array.png)
--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const path = require ("path");
 const app = express();
 const PORT = process.env.prot || 3001;
 
+const apiRoute = require("./routes/apiRoutes");
+const htmlRoute = require("./routes/htmlRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-// Get Route for homepage
-app.get("/", (req,res) =>
-res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+// // Get Route for homepage
+// app.get("/", (req,res) =>
+// res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
 
-// Get Route for notes page
-app.get("/notes", (req,res) =>
-res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+// // Get Route for notes page
+// app.get("/notes", (req,res) =>
+// res.sendFile(path.join(__dirname, "/public/notes.html"))
+// );
 
+app.use(apiRoute);
+app.use(htmlRoute);
 
 app.listen(PORT, () => {
     console.log(`app server is listening on http://localhost:${PORT}`);
