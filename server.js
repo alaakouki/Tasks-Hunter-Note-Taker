4f6d982eae595cff33d3f7ea6c4d88f219928349
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-03-20 15:30:14 -0700

    updating PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const path = require ("path");
 const app = express();
-const PORT = process.env.prot || 3001;
+const PORT = process.env.PORT || 5000;
 
 const apiRoute = require("./routes/apiRoutes");
 const htmlRoute = require("./routes/htmlRoutes");
